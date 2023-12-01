f24b296df71d99b44a71ba8e37a225e81414f58e
pochi <pochi>
2023-12-01 10:25:32 +0100

    WIP test for reduction

--- src/test/java/com/parkit/parkingsystem/FareCalculatorServiceTest.java
+++ src/test/java/com/parkit/parkingsystem/FareCalculatorServiceTest.java
@@ -152,4 +152,18 @@ public class FareCalculatorServiceTest {
         assertEquals(0,ticket.getPrice());
     }
 
+    @Test
+    public void calculateFareCarWithDiscount(){
+        Date inTime = new Date();
+        inTime.setTime(System.currentTimeMillis() - (60*60*1000));//1 hour in the parking
+        Date outTime = new Date();
+        ParkingSpot parkingSpot = new ParkingSpot(1,ParkingType.CAR,true);
+
+        ticket.setInTime(inTime);
+        ticket.setOutTime(outTime);
+        ticket.setParkingSpot(parkingSpot);
+        fareCalculatorService.calculateFare(ticket);
+        assertEquals((1*Fare.CAR_RATE_PER_HOUR),ticket.getPrice());
+    }
+
 }
